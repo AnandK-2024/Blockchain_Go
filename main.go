59d3cb80b061de329f2e53a40ae1bbec73fcd476
AnandK-2024 <anandkumar@iitbhilai.ac.in>
2023-10-08 22:15:06 +0530

    working on server

--- main.go
+++ main.go
@@ -2,9 +2,32 @@ package main
 
 import (
 	"fmt"
+	"time"
+	// "github.com/AnandK-2024/Blockchain/network"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
+// func main() {
+// 	fmt.Println("Hello, World!")
+// 	//  Create a new LocalTransport instance
+// 	transport := network.NewLocaltransport(network.NetAddr("localhost"))
+// 	opts := network.serveropts{
+// 		transport: []network.Transport{transport} ,
+// 	}
+// 	network.Newserver(opts)
 
+// }
+
+func printMessage(message string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(message)
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	defer go printMessage("Hello")
+	go printMessage("World")
+
+	// Wait for goroutine to complete
+	time.Sleep(5 * time.Second)
 }
--- network/server.go
+++ network/server.go
@@ -1,4 +1,15 @@
 package network
 
+type serveropts struct {
+	transport []Transport
+}
+
 type server struct {
+	serveropts
+}
+
+func Newserver(opts serveropts) server {
+	return *&server{
+		serveropts: opts,
+	}
 }
